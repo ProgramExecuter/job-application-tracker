cc622dcc1f064884f16dc1c764e8236c2ee5f7a2
Amit Singh Rana <ranaa73000@gmail.com>
2022-08-07 21:02:35 +0530

    Basic Routing and Controlling

--- server/controllers/Job.js
+++ server/controllers/Job.js
@@ -1,5 +1,30 @@
+// Get all Jobs
 const getAllJobs = async (req, res) => {
   return res.status(200).json({ message: "All Jobs" });
 };
 
-export { getAllJobs };
+// Create new Job
+const createJob = async (req, res) => {
+  return res.status(200).json({ message: "Create new Job" });
+};
+
+// Get a particular Job
+const particularJob = async (req, res) => {
+  return res.status(200).json({ message: `Job with Id: ${req.params.jobId}` });
+};
+
+// Update a Job
+const updateJob = async (req, res) => {
+  return res
+    .status(200)
+    .json({ message: `Edit Job with Id: ${req.params.jobId}` });
+};
+
+// Delete a Job
+const deleteJob = async (req, res) => {
+  return res
+    .status(200)
+    .json({ message: `Delete Job with Id: ${req.params.jobId}` });
+};
+
+export { getAllJobs, createJob, particularJob, updateJob, deleteJob };
--- server/routes/Job.js
+++ server/routes/Job.js
@@ -1,9 +1,28 @@
 import express from "express";
 
-import { getAllJobs } from "../controllers/Job.js";
+import {
+  getAllJobs,
+  createJob,
+  particularJob,
+  updateJob,
+  deleteJob,
+} from "../controllers/Job.js";
 
 const router = express.Router();
 
+// Get all Jobs
 router.get("/", getAllJobs);
+
+// Create new Job
+router.post("/", createJob);
+
+// Get particular Job
+router.get("/:jobId", particularJob);
+
+// Update a Job
+router.patch("/:jobId", updateJob);
+
+// Delete a Job
+router.delete("/:jobId", deleteJob);
 
 export default router;
