440c0d19318e26af096cb363bbecdcfea0ba2afb
Amit Singh Rana <ranaa73000@gmail.com>
2022-08-16 21:42:57 +0530

    Added basic functionality of each route

--- server/controllers/Job.js
+++ server/controllers/Job.js
@@ -1,30 +1,48 @@
+import Job from "../models/Job.js";
+
 // Get all Jobs
 const getAllJobs = async (req, res) => {
-  return res.status(200).json({ message: "All Jobs" });
+  // List of all jobs
+  const jobs = await Job.find();
+
+  return res.status(200).json(jobs);
 };
 
 // Create new Job
 const createJob = async (req, res) => {
-  return res.status(200).json({ message: "Create new Job" });
+  // Create new Job instance and save it
+  const newJob = new Job(req.body);
+  await newJob.save();
+
+  return res.status(201).json(newJob);
 };
 
 // Get a particular Job
 const particularJob = async (req, res) => {
-  return res.status(200).json({ message: `Job with Id: ${req.params.jobId}` });
+  // Find the job
+  const foundJob = await Job.findById(req.params.jobId);
+
+  return res.status(200).json(foundJob);
 };
 
 // Update a Job
 const updateJob = async (req, res) => {
+  // Find and update Job
+  await Job.findByIdAndUpdate(req.params.jobId, { $set: req.body });
+
   return res
     .status(200)
-    .json({ message: `Edit Job with Id: ${req.params.jobId}` });
+    .json({ message: `Updated Job with Id: ${req.params.jobId}` });
 };
 
 // Delete a Job
 const deleteJob = async (req, res) => {
+  // Find the job and delete it
+  await Job.findByIdAndDelete(req.params.jobId);
+
   return res
     .status(200)
-    .json({ message: `Delete Job with Id: ${req.params.jobId}` });
+    .json({ message: `Deleted Job with Id: ${req.params.jobId}` });
 };
 
 export { getAllJobs, createJob, particularJob, updateJob, deleteJob };
